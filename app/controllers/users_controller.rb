63fb697cd0bb8b940636a22b4fc603bf53cc2c32
Masato <144527087+3-masato@users.noreply.github.com>
2023-10-16 12:23:20 +0000

    Updated layouts, edit user profile, routing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,20 @@ class UsersController < ApplicationController
   end
 
   def index
+    @user = current_user
     @users = User.all
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :introduction ,:profile_image)
   end
 end
